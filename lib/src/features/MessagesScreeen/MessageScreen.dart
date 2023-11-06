8e3bbcb24823ae0c5657aea75fc51e103a4cb7a8
zain2127 <zainabid234@gmail.com>
2023-11-06 22:51:50 +0500

    messages promotion and wallet screen ui completed

--- lib/src/features/MessagesScreeen/MessageScreen.dart
+++ lib/src/features/MessagesScreeen/MessageScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:influencer/src/Utils/CustomSearchbar.dart';
+import 'package:influencer/src/Utils/InboxCard.dart';
 
 class MessageScreen extends StatefulWidget {
   const MessageScreen({Key? key}) : super(key: key);
@@ -10,6 +12,35 @@ class MessageScreen extends StatefulWidget {
 class _MessageScreenState extends State<MessageScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0XFF97CD9A),
+        title: Text('Messages'),
+        centerTitle: true,
+        
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal:16.0,vertical: 8.0),
+        child: SizedBox(
+          height: MediaQuery.sizeOf(context).height,
+          width: MediaQuery.sizeOf(context).width,
+            child: Column(
+              children: [
+                CustomSearchField(),
+                SizedBox(height: 20,),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 20,
+                    itemBuilder: (
+                    BuildContext context, index) {
+                    return const Inboxcard(image: 'assets/images/celebrity.png', title_text: 'Emma' ,subtitle_text: 'Hello mam, hope you are good .I hava do',);
+                  }
+                  ),
+                ),
+              ],
+            ),
+        ),
+      ),
+    );
   }
 }
